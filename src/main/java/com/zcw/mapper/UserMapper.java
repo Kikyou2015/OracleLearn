d012511e789eb0cbc7037c2f32a0f9c440b3f4af
Kikyou <chaowei110@qq.com>
2017-11-10 08:58:51 +0800

    UserMapper

--- src/main/java/com/zcw/mapper/UserMapper.java
+++ src/main/java/com/zcw/mapper/UserMapper.java
@@ -13,21 +13,21 @@ import com.zcw.entity.UserEntity;
 
 public interface UserMapper {
 
-	@Select("SELECT * FROM USER_T")
-	@Results({ @Result(property = "utId", column = "UT_ID", javaType = Long.class),
-			@Result(property = "utName", column = "UT_NAME", javaType = String.class),
-			@Result(property = "utEmail", column = "UT_EMAIL", javaType = String.class),
-			@Result(property = "utAddr", column = "UT_ADDR", javaType = String.class),
-			@Result(property = "utGender", column = "UT_GENDER", javaType = Short.class),
-			@Result(property = "utOrgid", column = "UT_ORGID", javaType = String.class) })
-	List<UserEntity> getAll();
+    @Select("SELECT * FROM USER_T")
+    @Results({@Result(property = "utId", column = "UT_ID", javaType = Long.class),
+            @Result(property = "utName", column = "UT_NAME", javaType = String.class),
+            @Result(property = "utEmail", column = "UT_EMAIL", javaType = String.class),
+            @Result(property = "utAddr", column = "UT_ADDR", javaType = String.class),
+            @Result(property = "utGender", column = "UT_GENDER", javaType = Short.class),
+            @Result(property = "utOrgid", column = "UT_ORGID", javaType = String.class)})
+    List<UserEntity> getAll();
 
-	@Insert("INSERT INTO USER_T (UT_ID,UT_NAME,UT_EMAIL,UT_ADDR,UT_GENDER,UT_ORGID) VALUES (seq_user.nextval,#{utName},#{utEmail},#{utAddr},#{utGender},#{utOrgid})")
-	void insert(UserEntity user);
+    @Insert("INSERT INTO USER_T (UT_ID,UT_NAME,UT_EMAIL,UT_ADDR,UT_GENDER,UT_ORGID) VALUES (seq_user.nextval,#{utName},#{utEmail},#{utAddr},#{utGender},#{utOrgid})")
+    void insert(UserEntity user);
 
-	@Update("UPDATE USER_T SET UT_NAME=#{utName},UT_EMAIL=#{utEmail},UT_ADDR=#{utAddr},UT_GENDER=#{utGender},UT_ORGID=#{utOrgid} WHERE UT_ID = #{utId}")
-	void update(UserEntity user);
+    @Update("UPDATE USER_T SET UT_NAME=#{utName},UT_EMAIL=#{utEmail},UT_ADDR=#{utAddr},UT_GENDER=#{utGender},UT_ORGID=#{utOrgid} WHERE UT_ID = #{utId}")
+    void update(UserEntity user);
 
-	@Delete("DELETE FROM USER_T WHERE UT_ID = #{id}")
-	void delete(Long id);
+    @Delete("DELETE FROM USER_T WHERE UT_ID = #{id}")
+    void delete(Long id);
 }
